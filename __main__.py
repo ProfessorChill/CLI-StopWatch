57ce433e3b6122b3be6a1555735b8206a55c156c
Luna Zarus <lunakiraizarus@chilly.hitronhub.home>
2017-04-14 14:05:08 -0600

    Second Commit

---
+++ __main__.py
@@ -0,0 +1,65 @@
+from time import sleep
+from curses import wrapper
+
+import curses
+import threading
+
+
+class StopWatch(threading.Thread):
+    seconds = 0
+    minutes = 0
+    hours = 0
+
+    def run(self):
+        while True:
+            sleep(1)
+            StopWatch.seconds = StopWatch.seconds + 1
+            if StopWatch.seconds == 60:
+                StopWatch.minutes = StopWatch.minutes + 1
+                StopWatch.seconds = 1
+            if StopWatch.minutes == 60:
+                StopWatch.hours = StopWatch.hours + 1
+                StopWatch.minutes = 1
+
+
+# The input here does not work
+class GetInput(threading.Thread):
+    def run(self):
+        while True:
+            c = main.stdscr.getch()
+            if c == 27:
+                break
+        exit(main.stdscr)
+
+
+def main(stdscr):
+    curses.echo()
+    StopWatch().start()
+    while True:
+        sleep(0.10)
+        stdscr.clear()
+        strToDisp = '{} Seconds | {} Minutes | {} Hours'
+        strToDisp = strToDisp.format(StopWatch.seconds,
+                                     StopWatch.minutes,
+                                     StopWatch.hours)
+        stdscr.addstr(0, 0, strToDisp)
+        stdscr.refresh()
+        c = stdscr.getch()
+        if c == 27:
+            break
+    exit(stdscr)
+
+
+def exit(stdscr):
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+if __name__ == '__main__':
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+    stdscr.nodelay(1)
+    wrapper(main)
