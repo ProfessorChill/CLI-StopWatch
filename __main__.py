67e38df4129f26d7be4c04265c863af15cfab4d4
Luna Zarus <chilly@chilly.localdomain>
2017-04-17 11:24:00 -0600

    Added the ability to have multiple stopwatches, deletion is still rudimentary

--- __main__.py
+++ __main__.py
@@ -7,20 +7,24 @@ from curses import wrapper
 
 
 class StopWatch(threading.Thread):
-    seconds = 0
-    minutes = 0
-    hours = 0
+    seconds = []
+    minutes = []
+    hours = []
+    seconds.append(0)
+    minutes.append(0)
+    hours.append(0)
 
     def run(self):
         while MainProgram.running:
             sleep(1)
-            StopWatch.seconds = StopWatch.seconds + 1
-            if StopWatch.seconds == 60:
-                StopWatch.minutes = StopWatch.minutes + 1
-                StopWatch.seconds = 1
-            if StopWatch.minutes == 60:
-                StopWatch.hours = StopWatch.hours + 1
-                StopWatch.minutes = 1
+            for x in range(0, len(StopWatch.seconds)):
+                StopWatch.seconds[x] = StopWatch.seconds[x] + 1
+                if StopWatch.seconds[x] == 60:
+                    StopWatch.minutes[x] = StopWatch.minutes[x] + 1
+                    StopWatch.seconds = 1
+                if StopWatch.minutes[x] == 60:
+                    StopWatch.hours[x] = StopWatch.hours[x] + 1
+                    StopWatch.minutes = 1
 
 
 class MainProgram:
@@ -33,15 +37,24 @@ class MainProgram:
         while True:
             sleep(0.10)
             stdscr.clear()
-            strToDisp = '{} Seconds | {} Minutes | {} Hours'
-            strToDisp = strToDisp.format(StopWatch.seconds,
-                                         StopWatch.minutes,
-                                         StopWatch.hours)
-            stdscr.addstr(0, 0, strToDisp)
+            for x in range(0, len(StopWatch.seconds)):
+                strToDisp = '{} Seconds | {} Minutes | {} Hours'
+                strToDisp = strToDisp.format(StopWatch.seconds[x],
+                                             StopWatch.minutes[x],
+                                             StopWatch.hours[x])
+                stdscr.addstr(x, 0, strToDisp)
             stdscr.refresh()
             c = stdscr.getch()
             if c == 27:
                 break
+            elif c == 259:
+                StopWatch.seconds.append(0)
+                StopWatch.minutes.append(0)
+                StopWatch.hours.append(0)
+            elif c == 258:
+                StopWatch.seconds.pop(len(StopWatch.seconds)-1)
+                StopWatch.minutes.pop(len(StopWatch.minutes)-1)
+                StopWatch.hours.pop(len(StopWatch.hours)-1)
         MainProgram.running = False
         sys.exit()
 
